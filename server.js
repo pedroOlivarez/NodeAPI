e9558acda33b6c9a8f8183f5002b6803986776a9
Pedro Olivarez <pedro.olivarezjr@gmail.com>
2019-11-26 22:59:00 -0600

    Controllers and middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 // Route Files
 const bootcamps = require("./routes/bootcamps");
 //Load env vars
@@ -7,8 +8,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-//Mount routers
+//DEV logging middlware
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
+//Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
