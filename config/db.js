16853421e2cdddff10ddb7c85c701d3bad861825
Pedro Olivarez <pedro.olivarezjr@gmail.com>
2019-11-27 23:21:50 -0600

    Connected to Mongo

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const connectWithTimeout = ms => {
+   const ourPromise = new Promise(async (resolve, reject) => {
+      try {
+         setTimeout(() => {
+            reject(new Error(`Connecting took longer than ${ms}ms`));
+         }, ms);
+         const result = await mongoose.connect(process.env.MONG_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+         });
+         resolve(result);
+      } catch (err) {
+         reject(err);
+      }
+   });
+   return ourPromise;
+};
+
+const connectDB = async () => {
+   const conn = await connectWithTimeout(5000);
+   console.log(`MongoDB Connected: ${conn.connection.host}`.magenta.underline.bold);
+};
+
+module.exports = connectDB;
