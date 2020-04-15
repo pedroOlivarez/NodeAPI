bfe896c6785c98596b514481fffaebb423f4a6b1
Pedro Olivarez <pedro.olivarezjr@gmail.com>
2020-04-15 01:38:19 -0500

    Develop: error middleware cleanup

--- middleware/error.js
+++ middleware/error.js
@@ -2,29 +2,26 @@ const ErrorResponse = require("../utils/errorResponse");
 
 function errorHandler(err, req, res, next) {
    let error = { ...err };
+   let message, statusCode;
    console.log(err.stack.red);
 
    if (err.name === "CastError") {
-      const message = `${err.value} is not a valid Id`;
-      error = new ErrorResponse(message, 400);
-   }
-
-   if (err.code && err.code === 11000) {
-      const message = `Duplicate field value error. Error Message: ${err.errmsg}`;
-      error = new ErrorResponse(message, 400);
-   }
-
-   if (err.name === "ValidationError") {
-      const message = Object.values(err.errors)
+      message = `${err.value} is not a valid Id`;
+      statusCode = 400;
+   } else if (err.name === "ValidationError") {
+      message = Object.values(err.errors)
          .map(e => e.message)
-         .join(". ");
-      error = new ErrorResponse(message, 400);
+         .join("||");
+      statusCode = 400;
+   } else if (err.code === 11000) {
+      message = `Duplicate field value error. Error Message: ${err.errmsg}`;
+      statusCode = 400;
+   } else if (err.statusCode === 404) {
+      message = err.message;
+      statusCode = 404;
    }
 
-   if (err.statusCode === 404) {
-      const message = err.message;
-      error = new ErrorResponse(message, 404);
-   }
+   error = new ErrorResponse(message, statusCode);
 
    res.status(error.statusCode || 500).json({
       success: false,
