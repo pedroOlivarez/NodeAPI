34975fb84f058442a07015665692ce65357d0f5d
Pedro Olivarez <pedro.olivarezjr@gmail.com>
2020-04-14 23:58:24 -0500

    Develop: Error Handling

--- middleware/error.js
+++ middleware/error.js
@@ -8,9 +8,22 @@ function errorHandler(err, req, res, next) {
       const message = `${err.value} is not a valid Id`;
       error = new ErrorResponse(message, 400);
    }
+
+   if (err.code && err.code === 11000) {
+      const message = `Duplicate field value error. Error Message: ${err.errmsg}`;
+      error = new ErrorResponse(message, 400);
+   }
+
+   if (err.name === "ValidationError") {
+      const message = Object.values(err.errors)
+         .map((e) => e.message)
+         .join(". ");
+      error = new ErrorResponse(message, 400);
+   }
+
    res.status(error.statusCode || 500).json({
       success: false,
-      message: error.message || "Server error"
+      message: error.message || "Server error",
    });
 }
 
