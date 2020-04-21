3dc8b92103389e43e2c0896029ca5ee0bab7d79c
Pedro Olivarez <pedro.olivarezjr@gmail.com>
2020-04-21 00:29:05 -0500

    Develop: code formatting changes

--- middleware/async.js
+++ middleware/async.js
@@ -8,12 +8,17 @@ function asyncHandler(fn) {
       if (req.query) {
          let queryStr;
          const ourQuery = {...req.query};
+
          excludedFields.forEach(param => delete ourQuery[param]);
+
          queryStr = JSON.stringify(ourQuery);
          queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
          if (req.query.select) req.select = req.query.select.replace(/,/g, ' ');
+
          if(req.query.sort) req.sort = req.query.sort.replace(/,/g, ' ');
          else req.sort = '-createdAt';
+         
          req.query = JSON.parse(queryStr);
       }
       return Promise.resolve(fn(req, res, next)).catch(next);
