46817cd274610e6e99818d7475ede741105172e7
Pedro Olivarez <pedro.olivarezjr@gmail.com>
2020-05-26 02:33:08 -0500

    Develop: NodeAPI more code clean up

--- server.js
+++ server.js
@@ -6,30 +6,21 @@ const colors = require('colors');
 const fileUpload = require('express-fileupload');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
-
-//Load env vars
-dotenv.config({ path: './config/config.env' });
-
-//Connect to database
-connectDB();
-
-// Route Files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
-const app = express();
 
-// Body parser
-app.use(express.json());
+dotenv.config({ path: './config/config.env' });
+connectDB();
+
+const app = express();
 
 //DEV logging middlware
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-// File uploader
+app.use(express.json());
 app.use(fileUpload());
-// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 
@@ -38,10 +29,8 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold));
 
-// Handle unhandled rejections
 process.on('unhandledRejection', (err, promise) => {
    console.log(`Unhandled Error: ${err.message}`.red);
-   // Close server & exit process
    server.close(() => {
       process.exit(1);
    });
