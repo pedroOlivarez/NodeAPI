b2c303b9b451b43a698f7130b2c05c2093e6f26b
Pedro Olivarez <pedro.olivarezjr@gmail.com>
2020-06-20 23:24:14 -0500

    Develop-NodeAPI: Course ownership

--- controllers/courses.js
+++ controllers/courses.js
@@ -3,6 +3,7 @@ const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const { status } = require('../enums/responseStatus');
+const { roles } = require('../enums/roles');
 const success = true;
 
 // @desc Get all courses
@@ -40,13 +41,18 @@ exports.getCourse = asyncHandler(async(req, res, next) => {
 // @access Private
 exports.addCourse = asyncHandler(async(req, res, next) => {
    req.body.bootcamp = req.params.bootcampId;
+   req.body.user = req.user.id;
    
    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+   let errResponse;
 
    if (!bootcamp) {
-      const errorResponse = new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`, status.error.NOT_FOUND);
-      return next(errorResponse);
+      errResponse = new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`, status.error.NOT_FOUND);
+   } else if (req.user.role !== roles.ADMIN && bootcamp.user.toString() !== req.user.id) {
+      errResponse = new ErrorResponse('User is not authorized to add a course to this bootcamp.', status.error.UNAUTHORIZED);
    }
+
+   if (errResponse) return next(errResponse);
 
    const course = await Course.create(req.body);
 
@@ -63,11 +69,15 @@ exports.addCourse = asyncHandler(async(req, res, next) => {
 // @access Private
 exports.updateCourse = asyncHandler(async(req, res, next) => {
    let course = await Course.findById(req.params.id);
+   let errResponse;
    
    if (!course) {
-      const errorResponse = new ErrorResponse(`No course found with the id of ${req.params.id}`, status.error.NOT_FOUND);
-      return next(errorResponse);
+      errResponse = new ErrorResponse(`No course found with the id of ${req.params.id}`, status.error.NOT_FOUND);
+   } else if (req.user.role !== roles.ADMIN && course.user.toString() !== req.user.id) {
+      errResponse = new ErrorResponse('User not authorized to update this course.', status.error.UNAUTHORIZED);
    }
+
+   if (errResponse) return next(errResponse);
 
    course = await Course
       .findByIdAndUpdate(
@@ -88,12 +98,16 @@ exports.updateCourse = asyncHandler(async(req, res, next) => {
 // @route DELETE /api/v1/courses/:id
 // @access Private
 exports.deleteCourse = asyncHandler(async(req, res, next) => {
-   const course = await Course.findById(id);
+   const course = await Course.findById(req.params.id);
+   let errResponse;
    
    if (!course) {
-      const errorResponse = new ErrorResponse(`No course found with the id of ${req.params.id}`, status.error.NOT_FOUND);
-      return next(errorResponse);
+      errResponse = new ErrorResponse(`No course found with the id of ${req.params.id}`, status.error.NOT_FOUND);
+   } else if (req.user.role !== roles.ADMIN && course.user.toString() !== req.user.id) {
+      errResponse = new ErrorResponse('User not authorized to delete this course.', status.error.UNAUTHORIZED);
    }
+
+   if (errResponse) return next(errResponse);
 
    await course.remove()
 
